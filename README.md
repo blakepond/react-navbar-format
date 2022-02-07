10db6c6da64f294449e54f2914341ce2102bafc9
Blake Pond <blakehp@icloud.com>
2022-02-07 15:22:35 -0700

    youtube added, blog icon added, name added.

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
+<!-- Dev Env -->
+
 npm start should get ya started
+
+<!-- Deploying to internets -->
+
+delete build folder
+npm run build
+firebase deploy
+
+<!-- FontAwesome link for icons -->
+
+github.com/FortAwesome/Font-Awesome
---
+++ src/firebase/index.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+   apiKey:
+      "AIzaSyDYGW3uRJ1aDC9h2wVMliDEi4i0S2NEm8o",
+   authDomain:
+      "blakepond-portfolio.firebaseapp.com",
+   projectId: "blakepond-portfolio",
+   storageBucket:
+      "blakepond-portfolio.appspot.com",
+   messagingSenderId: "179261699255",
+   appId: "1:179261699255:web:638a991b28ed3ad6730932",
+};
+
+const app = initializeApp(firebaseConfig);
